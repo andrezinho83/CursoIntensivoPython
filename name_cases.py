784381c5dad0a9fd179428bf91994e8dff2cb87e
Andre Luiz Pimentel <andrezinho83@gmail.com>
2018-05-19 15:09:41 -0400

    First commit.

---
+++ name_cases.py
@@ -0,0 +1,6 @@
+nome = " andre luiz pimentel "
+print("Ola, " + nome.title().strip() + "!")
+
+famous_person = " albert einstein "
+message = "Uma pessoa que nunca cometeu um erro jamais tentou nada novo."
+print(famous_person.title().strip() + " certa vez disse: \n\t" + message)
